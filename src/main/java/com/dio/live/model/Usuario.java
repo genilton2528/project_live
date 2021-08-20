15c92afbe866206fdc49c8a17ad8964652554ca9
Genilton <geniltonbarbosadasilva@gmail.com>
2021-08-19 23:39:01 -0300

    fazendo models

---
+++ src/main/java/com/dio/live/model/Usuario.java
@@ -0,0 +1,27 @@
+package com.dio.live.model;
+
+import lombok.*;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Usuario {
+    private long id;
+    private String nome;
+    private BigDecimal tolerancia;
+    private LocalDateTime finalJornada;
+    @ManyToOne
+    private CategoriaUsuario categoriaUsuario;
+    @ManyToOne
+    private Empresa empresa;
+    @ManyToOne
+    private NivelAcesso nivelAcesso;
+    @ManyToOne
+    private JornadaTrabalho jornadaTrabalho;
+}
